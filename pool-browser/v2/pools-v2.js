556cdbac46eb5fdca8f96a6a84a61a416f50fe1e
Serguei Khasimkhanov <serkhas@hotmail.com>
2019-06-02 02:19:44 -0400

    automatically select today's date when visiting website

--- pool-browser/v2/pools-v2.js
+++ pool-browser/v2/pools-v2.js
@@ -1,9 +1,38 @@
 // globals that are filled in on page load
-let selectDate;
+let elSelectDate;
 
-function onSelectDate(event) {
-    // get the date they chose
-    let date = event.target.value;
+function getDate() {
+    let today = new Date();
+    let dd = today.getDate();
+
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+function selectDate(date) {
+    let el = elSelectDate.querySelector(`option[value='${date}']`);
+    if (el === null) {
+        console.log(`Error: Cannot find date '${date}'.`);
+        return;
+    }
+
+    if (elSelectDate.value !== date) {
+        console.log(`Manually change select date value to ${date}.`);
+        elSelectDate.value = date;
+    }
+
+    // Ight here we go
+
     let datecls = `date-${date}`;
 
     // hide old times
@@ -27,9 +56,19 @@ function onSelectDate(event) {
     }
 }
 
+function onSelectDate(event) {
+    // get the date they chose
+    let date = event.target.value;
+
+    // change to it
+    selectDate(date);
+}
+
 window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
 
-    selectDate = document.querySelector('#date-select');
-    selectDate.addEventListener('change', onSelectDate);
+    elSelectDate = document.querySelector('#date-select');
+    elSelectDate.addEventListener('change', onSelectDate);
+
+    selectDate(getDate());
 });
